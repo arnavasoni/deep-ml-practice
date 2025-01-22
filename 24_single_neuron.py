f1047fbca9ff0fe88467ee93efad05d9f1731166
Arnava Soni <arnavasoni1@gmail.com>
2025-01-22 22:40:15 +0530

    Latest Commit

--- 24_single_neuron.py
+++ 24_single_neuron.py
@@ -1,23 +1,17 @@
 # Single neuron
 import math
+
 def single_neuron_model(features: list[list[float]], labels: list[int], weights: list[float], bias: float) -> (list[float], float):
-    
-    probabilities = []
+	probabilities = [] # Results of different samples after each probability goes through sigmoid function
+	z = 0
+	for feature_vector in features:
+		z = sum(weight * feature for weight, feature in zip(weights, feature_vector)) + bias
+		prob = 1 / (1 + math.exp(-z))
+		probabilities.append(round(prob, 4))
 
-    # single neuron
-    for feature_vector in features:
-        # calculate z
-        z = sum(weight * feature for weight, feature in zip(weights, feature_vector)) + bias
-
-        # calculate probability, and use sigmoid, cuz binary classification
-        prob = 1 / (1 + math.exp(-z))
-        probabilities.append(round(prob, 4))
-    
-    # mean square error
-    mse = sum((prob - label) ** 2 for prob, label in zip(probabilities, labels)) / len(labels)
-    mse = round(mse, 4)
-
-    return probabilities, mse
+	mse = sum((prob - label) ** 2 for prob, label in zip(probabilities, labels)) / len(labels)
+	mse = round(mse, 4)
+	return probabilities, mse
  
 features = [[0.5,1.0],
             [-1.5, -2.0],
